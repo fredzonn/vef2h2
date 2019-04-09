680f8edbf1bc2609ea27ea072901759b591ad7d7
steini96 <steini.tki@gmail.com>
2019-04-09 14:11:10 +0000

    Ekkert

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <header className="header">
       <div className="header__content">
         <h1 className="header__title">
-          <Link className="header__titleLink" to="/">Vefforritunarbúðin</Link>
+          <Link className="header__titleLink" to="/"> Vefforritunarbúðin </Link>
         </h1>
         <NavLink activeClassName="header__link--selected" exact to="/cart">Karfa</NavLink>
       </div>
