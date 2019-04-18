3d2dded82d40eda26d8431546a11040301a11cfe
unknown <ema22@hi.is>
2019-04-18 23:00:40 +0000

    fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,15 @@
-import { IProduct } from './types';
+import { IProduct, ICategory } from './types';
+import { number } from 'prop-types';
 
 // Sækja slóð á API úr env
-const baseurl: string | undefined = process.env.REACT_APP_API_URL;
+const baseurl:string | undefined = process.env.REACT_APP_API_URL;
+console.log(baseurl);
 
-async function getProduct(id: number | string): Promise<IProduct> {
-  const url = new URL('/', baseurl);
+async function getProduct(id: number | string) : Promise<IProduct> {
+  const url = new URL('/products/'+id, baseurl);
   const response = await fetch(url.href)
   const data = await response.json();
+  console.log("data úr index.ts: ",data);
 
   const product: IProduct = {
     category: {
@@ -20,6 +23,107 @@ async function getProduct(id: number | string): Promise<IProduct> {
   };
 
   return product;
+}
+
+async function getProducts(offset:Number, limit:Number) : Promise<IProduct[]> {
+  const url = new URL('/products/', baseurl);//(/categories?offset=${offset}&limit=${limit},baseurl);
+  const response = await fetch(url.href);
+  const JSONgogn = response.json();
+  const arr:IProduct[] = [];
+
+  const prods = JSONgogn.then(function(data){
+    console.log("datað er þetta: ",data);
+    data.items.forEach(function(element: { id: number; title: string; price: number; image: string; category: ICategory; }) {
+      const products: IProduct = {
+        id: element.id,
+        title: element.title,
+        price: element.price,
+        image: element.image,
+        category: element.category,
+      };
+      arr.push(products);
+    });
+    return arr;
+  });
+  return new Promise((resolve) => resolve(prods))
+}
+
+async function getCategor(/*id: number | string |undefined*/) /*: Promise<ICategory[]>*/ {
+  const url = new URL('/categories', baseurl);
+  const response = await fetch(url.href)
+  const data = await response.json();
+  //console.log("data úr index.ts: ",data.items);
+
+  //const prods: ICategory[] = await response.json();
+  //console.log("prod er: ", data.items);
+  /*const product: ICategory = {
+    id: data.items[1].id,
+    title: data.items[1].title,
+  };
+  console.log("product: ", product);*/
+  return data;
+}
+
+/*async function getCategories(offset:Number, limit:Number) : Promise<ICategory[]> {
+  // todo sækja vöru
+  const url = new URL('/categories/'+1, baseurl);//(/categories?offset=${offset}&limit=${limit},baseurl);
+  const response = await fetch(url.href);
+  const JSONgogn = response.json();
+  const arr:ICategory[] = [];
+
+  const cats = JSONgogn.then(function(data){
+    cats.forEach(function(element: { id: number; title: string; }) {
+      const category: ICategory = {
+        id: element.id,
+        title: element.title,
+      };
+      arr.push(category);
+    });
+    return arr;
+  });
+  return new Promise((resolve) => resolve(cats))
+}*/
+
+async function getCategories(offset:Number, limit:Number) : Promise<ICategory[]> {
+  // todo sækja vöru
+  const url = new URL('/categories/', baseurl);//(/categories?offset=${offset}&limit=${limit},baseurl);
+  const response = await fetch(url.href);
+  const JSONgogn = response.json();
+  const arr:ICategory[] = [];
+
+  const cats = JSONgogn.then(function(data){
+    console.log("datað er þetta: ",data);
+    data.items.forEach(function(element: { id: number; title: string; }) {
+      const category: ICategory = {
+        id: element.id,
+        title: element.title,
+      };
+      arr.push(category);
+    });
+    return arr;
+  });
+  return new Promise((resolve) => resolve(cats))
+}
+
+async function getCategory(category:String) : Promise<ICategory[]> {
+  // todo sækja vöru
+  const url = new URL('/products/'+'?category'+"'"+category+"'", baseurl);//new URL('/categories/', baseurl);//(/categories?offset=${offset}&limit=${limit},baseurl);
+  const response = await fetch(url.href);
+  const JSONgogn = response.json();
+  const arr:ICategory[] = [];
+
+  const cats = JSONgogn.then(function(data){
+    console.log("gögn er þetta: ",data);
+    data.items.forEach(function(element: { id: number; title: string; }) {
+      const category: ICategory = {
+        id: element.id,
+        title: element.title,
+      };
+      arr.push(category);
+    });
+    return arr;
+  });
+  return new Promise((resolve) => resolve(cats))
 }
 
 interface options {
@@ -52,5 +156,11 @@ async function post(endpoint: string, data: any) {
 }
 
 export {
-  getProduct, post
+  getProduct,
+  getProducts,
+  getCategories,
+  getCategor,
+  getCategory,
+  post,
+
 };
