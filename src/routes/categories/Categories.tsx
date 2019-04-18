a85fec55c38c05589558030a957a92948945efce
unknown <ema22@hi.is>
2019-04-18 23:02:02 +0000

    fix2

--- src/routes/categories/Categories.tsx
+++ src/routes/categories/Categories.tsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import { getProduct, getCategories, getCategor } from '../../api/index';
+import React, { Fragment, useState, useEffect } from 'react';
+import Helmet from 'react-helmet';
+import { IProduct, ICategory } from '../../api/types';
 
 export default function CategoriesRoute() {
+
+  const [categories, setCategories] = useState();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const result = await getCategories(0, 50);
+      setCategories(result);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  async function onClick(index: Number) {
+    return location.href = "categories/"+index;
+  }
+
+  function fall(categories: ICategory[] | undefined) {
+    if (categories !== undefined) {
+      return (
+
+        <div className="categories">
+
+          {categories.map((data, i) => (
+            <div key={i} className="category">
+
+              <div className="categoryinn" key={data.id || i} onClick={() => onClick(data.id)}>
+                <p>{data.title}</p>
+              </div>
+
+            </div>
+          ))}
+
+        </div>
+
+      );;
+    } else {
+      return '';
+    }
+  }
+
   return (
-    <p>categories</p>
-  );
+  <div className="container">
+  <Helmet title="Flokkar" />
+    {loading && (
+        <h2 className="loading">Hleð gögnum...</h2>
+    )}
+    {!loading && (
+      <div className="haldari">
+        <h2>Skoðaðu vöruflokkana okkar</h2>
+        {fall(categories)}
+      </div>
+    )}
+  </div>
+);
+
+
 }
